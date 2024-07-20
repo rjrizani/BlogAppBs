85d67ddbea0273d18a0ba538338a9cd3089dcfc8
rjrizani <rjrizani@gmail.com>
2024-07-20 10:39:10 +0800

    add event in submit button in contact.html then data wiil send to database

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import*
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root@127.0.0.1/BlogApp"
 db = SQLAlchemy(app)
 #pip install mysqlclient
+# pip3 install mysql-connector
+
 
 
 class Contact(db.Model):
@@ -29,12 +32,21 @@ def about():
 def login():
     return render_template("login.html")
 
-@app.route("/contact")
+@app.route("/contact",methods=['GET','POST'])
 def contact():
+    if (request.method=='POST'):
+        Name = request.form.get('name')
+        Email = request.form.get('email')
+        Msg = request.form.get('message')
+        entry = Contact(name=Name,email=Email, message=Msg, date=datetime.today().date())
+        db.session.add(entry)
+        db.session.commit()
     return render_template("contact.html")
 
 
 
 
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

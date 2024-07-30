1d5dabac0e14fa44785c17b034946abca920b917
rjrizani <rjrizani@gmail.com>
2024-07-30 15:42:49 +0800

    post 2

--- main.py
+++ main.py
@@ -30,14 +30,30 @@ class Contact(db.Model):
     message = db.Column(db.String(150), nullable=False)
     date = db.Column(db.String(20))
 
-
+class Posts(db.Model):
+    post_id = db.Column('post_id',db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    sub_title = db.Column(db.String(200))
+    location = db.Column(db.String(50))
+    author = db.Column(db.String(30))
+    date_posted = db.Column(db.Date)
+    image = db.Column(db.String(150))
+    content_1= db.Column(db.Text)
+    content_2 = db.Column(db.Text)
+    slug = db.Column(db.String(100), unique=True)
 
 @app.route("/")
 def home():
-    return render_template("index.html", param=param)
-@app.route("/post")
-def post():
-    return render_template("post.html", param=param)
+    db.session.commit()
+    post_data = Posts.query.all()
+
+    return render_template("index.html", posts=post_data, param=param)
+
+
+@app.route("/post/<slug>", methods=['GET'])
+def post(slug):
+    single_post = Posts.query.filter_by(slug=slug).first()
+    return render_template("post.html", post=single_post,param=param)
 
 @app.route("/about")
 def about():

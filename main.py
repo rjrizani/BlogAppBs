6e4e71516f249b441b440f9114e326be3ab110aa
rjrizani <rjrizani@gmail.com>
2024-08-22 22:36:20 +0800

    sign up and login

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template,request,redirect,url_for,session
+from flask import Flask, render_template,request,redirect,url_for,session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import*
 import json
 import math
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
 app = Flask(__name__)
 
@@ -16,7 +17,8 @@ else:
 
 SECRET_KEY = param["secret_key"]
 
-app.config['SECRET_KEY']
+#app.config['SECRET_KEY']
+app.config['SECRET_KEY']=param['secret_key']
 
 db = SQLAlchemy(app)
 #pip install mysqlclient
@@ -42,6 +44,13 @@ class Posts(db.Model):
     content_1= db.Column(db.Text)
     content_2 = db.Column(db.Text)
     slug = db.Column(db.String(100), unique=True)
+
+class Users(UserMixin,db.Model):
+    id = db.Column('id',db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
 @app.route("/")
 def home():
@@ -79,8 +88,18 @@ def post(slug):
 def about():
     return render_template("about.html", param=param)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if (request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = Users.query.filter_by(username=username, password=password).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Invalid credentials")
+            return redirect(url_for('login'))
     return render_template("login.html", param=param)
 
 @app.route("/contact",methods=['GET','POST'])
@@ -101,12 +120,73 @@ def dashboard():
     return render_template("admin/index.html", param=param, posts=posts, contacts=contacts)
 
 
-@app.route("/edit")
-def edithtml():
-    return render_template("admin/editPost.html", param=param)
+@app.route("/editPost/<string:post_id>",methods=['GET', 'POST'])
+def edithtml(post_id):
+    if request.method == 'POST':
+        ntitle = request.form.get('title')
+        nsubtitle = request.form.get('subtitle')
+        nauthor = request.form.get('author')
+        nimage = request.form.get('image')
+        nlocation = request.form.get('location')
+        nslug = request.form.get('slug')
+        ndate = datetime.now()
+        ncontent1 = request.form.get('content1')
+        ncontent2 = request.form.get('content2')
+        if post_id == '0':  # add a new post in db
+            post = Posts(title=ntitle, sub_title=nsubtitle, location=nlocation,
+                         content_1=ncontent1, content_2=ncontent2, author=nauthor, image=nimage, slug=nslug,
+                         date_posted=datetime.now())
+            db.session.add(post)
+            db.session.commit()
+        else:
+            post = Posts.query.filter_by(post_id=post_id).first()
+            post.title = ntitle
+            post.sub_title = nsubtitle
+            post.author = nauthor
+            post.image = nimage
+            post.location = nlocation
+            post.date_posted = ndate
+            post.content_1 = ncontent1
+            post.content_2 = ncontent2
+            post.slug = nslug
+            db.session.commit()
+        return redirect(url_for('dashboard'))
+    post = Posts.query.filter_by(post_id=post_id).first()
+    return render_template('admin/editPost.html', param=param, post=post, post_id=post_id)
 
 
+@app.route("/delete/<string:post_id>",methods=['GET', 'POST'])
+def delete(post_id):
+    post = Posts.query.filter_by(post_id=post_id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('dashboard'))
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if (request.method == 'POST'):
+        name = request.form.get('name')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists")
+            return redirect(url_for('signup'))
+        new_user = Users(name=name, username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    return render_template("signup.html", param=param)
+
+#login manager
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 
 if __name__=='__main__':
